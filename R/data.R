39f710fafe15bd9ec495487efdd53fc05bafae9c
Marius <marius.mather@sydney.edu.au>
2020-04-29 13:56:17 +1000

    Docs for universal_tags data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Universal part of speech tags, as used by the udpipe and spacy taggers
+#'
+#' A dataset mapping the tags produced by the POS taggers to
+#' either content or function words
+#'
+#'
+#' @format A data frame with 17 rows and 3 variables:
+#' \describe{
+#'   \item{Tag}{Part of speech tag}
+#'   \item{Name}{Full name for the tag}
+#'   \item{Type}{"Content" or "Function", or NA (missing) for non-words}
+#' }
+#' @source \url{https://universaldependencies.org/u/pos/index.html}
+"universal_tags"
