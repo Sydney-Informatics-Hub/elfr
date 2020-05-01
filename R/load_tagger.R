56220c8dfc15d4a6e0df39c7a5cb25b50fe969f7
Marius <marius.mather@sydney.edu.au>
2020-05-01 13:46:43 +1000

    Fix typo elfer -> elfr

--- R/load_tagger.R
+++ R/load_tagger.R
@@ -3,7 +3,7 @@
 #' @return A udpipe model, ready to be used in \code{\link[udpipe]{udpipe_annotate}}.
 load_tagger <- function() {
     model_path <- system.file("extdata", "english-ewt-ud-2.4-190531.udpipe",
-                              package="elfer")
+                              package="elfr")
     tagger <- udpipe::udpipe_load_model(model_path)
     tagger
 }
