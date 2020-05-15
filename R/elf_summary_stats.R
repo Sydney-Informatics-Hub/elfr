4307110b7144cba569fa0caebf9a00d9abb5893f
Marius <marius.mather@sydney.edu.au>
2020-05-15 10:46:15 +1000

    Make sure we assume at least 1 clause per sentence

--- R/elf_summary_stats.R
+++ R/elf_summary_stats.R
@@ -12,7 +12,9 @@
 #'     \itemize{
 #'         \item \code{n_words} Number of words
 #'         \item \code{n_content} Number of content words
-#'         \item \code{n_clauses} Number of clauses (estimated using number of verbs)
+#'         \item \code{n_clauses} Number of clauses (estimated using number of non-auxiliary verbs).
+#'            Part-of-speech tagging is imperfect, and may not identify verbs in all cases, so
+#'            we assume a minimum of 1 clause per sentence even when no verbs are identified.
 #'         \item \code{prop_content} Proportion of content words (0.0 to 1.0)
 #'         \item \code{prop_function} Proportion of function words (0.0 to 1.0)
 #'         \item \code{simple_density} Approximate lexical density, \code{prop_content} * 10
@@ -26,9 +28,22 @@
 elf_summary_stats <- function(texts, parser = "udpipe") {
     parser <- match.arg(parser, c("udpipe", "spacy"))
 
-    pos_tags <- get_pos_tags(texts, parser = parser)
+    pos_tags <- get_pos_tags(texts, parser = parser) %>%
+        # NOTE: the parsers assign each text an id like "text123" - because
+        #   these are character the numbers may not sort in order,
+        #   so make sure they are correctly ordered before grouping/summarising
+        dplyr::mutate(doc_id = forcats::fct_inorder(.data$doc_id))
+
+    # Count clauses (number of non-auxiliary verbs)
+    clause_counts <- pos_tags %>%
+        dplyr::group_by(.data$doc_id, .data$sentence_id) %>%
+        # Assume at least one clause per sentence
+        dplyr::summarise(sentence_clauses = max(1, sum(.data$upos == "VERB"))) %>%
+        dplyr::group_by(.data$doc_id) %>%
+        dplyr::summarise(n_clauses = sum(.data$sentence_clauses))
 
     pos_tags %>%
+        dplyr::left_join(clause_counts, by = "doc_id") %>%
         dplyr::group_by(.data$doc_id) %>%
         dplyr::summarise(
             # Number of words for the purpose of counting lexical density,
@@ -36,7 +51,7 @@ elf_summary_stats <- function(texts, parser = "udpipe") {
             n_words = sum(! is.na(.data$pos_type)),
             n_content = sum(.data$pos_type == "Content", na.rm = TRUE),
             n_function = sum(.data$pos_type == "Function", na.rm = TRUE),
-            n_clauses = sum(.data$upos == "VERB"),
+            n_clauses = dplyr::first(.data$n_clauses),
             prop_content = .data$n_content / .data$n_words,
             prop_function = .data$n_function / .data$n_words,
             # Multiply by 10 to approximate lexical density
@@ -45,6 +60,7 @@ elf_summary_stats <- function(texts, parser = "udpipe") {
         ) %>%
         # Make sure we ungroup
         dplyr::ungroup() %>%
+        # Include the original text
         dplyr::mutate(text = texts) %>%
         dplyr::select(.data$text, dplyr::everything(), - .data$doc_id)
 }
